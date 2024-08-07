dd711d24fdfceb78be1cbacd2525a81cc63f4324
ChoiYoungHa <askillofgod159@naver.com>
2024-08-07 18:04:03 +0900

    test

--- src/main/java/com/example/git_practice/controller/HelloController.java
+++ src/main/java/com/example/git_practice/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.git_practice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,12 @@ public class HelloController {
         return "Hello";
     }
 
-    public String test(){
+    public String test() {
         return "<h1>Test<h1>";
+    }
+    @GetMapping("/helloworld-ajh")
+    public String helloWorld() {
+        return "Hello World";
+
     }
 }
