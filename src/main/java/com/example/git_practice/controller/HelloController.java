aa31fce4ac8d65e1740ef81ad2d6de806773150e
jjeong1015 <shinin_2008@naver.com>
2024-08-07 17:15:22 +0900

    feat: HelloController 추가

--- src/main/java/com/example/git_practice/controller/HelloController.java
+++ src/main/java/com/example/git_practice/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.git_practice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class HelloController {
 
     public String test() {
         return "<h1>Test<h1>";
+    }
+
+    @GetMapping("/jjeong")
+    public String jjeongWorld() {
+        return "jjeong World";
     }
 }
