e8e68d2d93d90c82efd38efbeafd0d75afacb3e3
ChoiYoungHa <askillofgod159@naver.com>
2024-08-07 18:06:37 +0900

    test4

--- src/main/java/com/example/git_practice/controller/HelloController.java
+++ src/main/java/com/example/git_practice/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.git_practice.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,11 +25,18 @@ public class HelloController {
         return "jjeong World";
     }
 
+<<<<<<< HEAD
 
     @GetMapping("/abc")
     public String abc() {
         return "abc git test";
     }
 
+
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "Delete";
+    }
 
 }
