1eb1f8b0a22eed454f7d5f6882d86720e926df25
jjeong1015 <shinin_2008@naver.com>
2024-08-07 17:31:39 +0900

    feat: delete 추가

--- src/main/java/com/example/git_practice/controller/HelloController.java
+++ src/main/java/com/example/git_practice/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.git_practice.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class HelloController {
     @GetMapping("/jjeong")
     public String jjeongWorld() {
         return "jjeong World";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "Delete";
     }
 }
